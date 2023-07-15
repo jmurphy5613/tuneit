ab123a7548f080f45b8a83cb8cccae5032ef9768
jmurphy5613 <jmurphy5613@gmail.com>
2023-07-15 12:41:15 -0400

    fully set up animatino

--- components/songle-sign/SongleSign.tsx
+++ components/songle-sign/SongleSign.tsx
@@ -1,18 +1,33 @@
 import styles from './SongleSign.module.css'
 
 const SongleSign = () => {
+
+
     return (
         <div className={styles.container}>
             <div className={styles["letter-container"]}>
                 <h1 className={styles.letter}>S</h1>
             </div>
-            <div className={styles.block} style={{ left: '7.5px' }} />
-            {/* <h1 className={styles.letter}>O</h1>
-            <h1 className={styles.letter}>N</h1>
-            <div className={styles.block} style={{ left: '215px' }} />
-            <h1 className={styles.letter}>G</h1>
-            <h1 className={styles.letter}>L</h1>
-            <div className={styles.block} style={{ left: '322.5px' }} /> */}
+            <div className={styles.block1} />
+            <div className={styles["letter-container"]}>
+                <h1 className={styles["other-letter"]}>O</h1>
+            </div>
+            <div className={styles["letter-container"]}>
+                <h1 className={styles.letter}>N</h1>
+            </div>
+            <div className={styles.block2} />
+            <div className={styles["letter-container"]}>
+                <h1 className={styles["other-letter"]}>G</h1>
+            </div>
+            <div className={styles["letter-container"]}>
+                <h1 className={styles.letter}>L</h1>
+            </div>
+            <div className={styles.block3} />
+            <div className={styles["letter-container"]}>
+                <h1 className={styles["other-letter"]}>E</h1>
+            </div>
+
+
 
 
 
