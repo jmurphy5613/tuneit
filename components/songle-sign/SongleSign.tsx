cbc59bbe7350170d6819c73e8289c204360c313e
jmurphy5613 <jmurphy5613@gmail.com>
2023-07-15 12:12:19 -0400

    got a version of the text animation working

--- components/songle-sign/SongleSign.tsx
+++ components/songle-sign/SongleSign.tsx
@@ -3,6 +3,18 @@ import styles from './SongleSign.module.css'
 const SongleSign = () => {
     return (
         <div className={styles.container}>
+            <div className={styles["letter-container"]}>
+                <h1 className={styles.letter}>S</h1>
+            </div>
+            <div className={styles.block} style={{ left: '7.5px' }} />
+            {/* <h1 className={styles.letter}>O</h1>
+            <h1 className={styles.letter}>N</h1>
+            <div className={styles.block} style={{ left: '215px' }} />
+            <h1 className={styles.letter}>G</h1>
+            <h1 className={styles.letter}>L</h1>
+            <div className={styles.block} style={{ left: '322.5px' }} /> */}
+
+
 
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import styles from '@/styles/Home.module.css'
+import SongleSign from '@/components/songle-sign/SongleSign'
 
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
-
+			<SongleSign />
 		</div>
 	)
 }
