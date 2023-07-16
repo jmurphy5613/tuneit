f12e208e56a321036e13998fcb650a59de0bd3fe
jmurphy5613 <jmurphy5613@gmail.com>
2023-07-15 22:07:48 -0400

    added some files

--- utils/data.ts
+++ utils/data.ts
@@ -12,11 +12,15 @@ export const userItem: UserItem[] = [
         username: 'Cpfist',
         image: '/pfps/demo-pfp2.jpeg'
     },
+    {
+        username: 'jmurphy5613',
+        image: '/pfps/johntransparent.png'
+    }
 ]
 
 export const duplicatedUserItems = (users: UserItem[]) => {
     let duplicatedUsers: UserItem[] = []
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 5; i++) {
         duplicatedUsers = duplicatedUsers.concat(users)
     }
     return duplicatedUsers
