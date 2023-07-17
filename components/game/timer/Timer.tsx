3c036316540381382e2d7e26f4b948f4bc3afac0
jmurphy5613 <jmurphy5613@gmail.com>
2023-07-17 12:59:48 -0400

    set up proper timer and set up helper functions for styling

--- components/game/timer/Timer.tsx
+++ components/game/timer/Timer.tsx
@@ -3,20 +3,40 @@ import { useState, useEffect } from 'react'
 
 const Timer = () => {
 
-    const [seconds, setSeconds] = useState<number>(0)
+    const [seconds, setSeconds] = useState<number>(60)
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setSeconds(seconds => seconds + 1)
+        const timer = setInterval(() => {
+            setSeconds(prevSeconds => {
+                if (prevSeconds === 0) {
+                    clearInterval(timer);
+                    return 0;
+                } else {
+                    return prevSeconds - 1;
+                }
+            });
         }, 1000)
-        return () => clearInterval(interval)
+        return () => clearInterval(timer)
     }, [])
+
+    const getPercentage = () => {
+        return (seconds / 60) * 100
+    }
+
+    const getProgressBarBackground = () => {
+        const red = Math.round((100 - getPercentage()) * 2.55); // Interpolate red channel
+        const green = Math.round(getPercentage() * 2.55); // Interpolate green channel
+  
+        const rgbColor = `rgb(${red}, ${green}, 0)`;
+        return rgbColor;
+    }
+
 
     return (
         <div className={styles.container}>
-            <h2>{seconds}</h2>
+            <h2 className={styles.seconds}>{seconds}</h2>
             <div className={styles.bar}>
-                <div className={styles.progress} />
+                <div className={styles.progress} style={{ width: `${getPercentage()}%`, backgroundColor: getProgressBarBackground() }} />
             </div>
         </div>
     )
--- pages/play.tsx
+++ pages/play.tsx
@@ -1,10 +1,12 @@
 import styles from '../styles/Game.module.css'
 import SongleSign from '@/components/home/songle-sign/SongleSign'
+import Timer from '@/components/game/timer/Timer'
 
 const Play = () => {
     return (
         <div className={styles.container}>
             <SongleSign notAnimated />
+            <Timer />
         </div>
     )
 }
