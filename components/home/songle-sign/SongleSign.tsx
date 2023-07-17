da6e83060ec5f965bb48e3e679e76abb9ac8deea
jmurphy5613 <jmurphy5613@gmail.com>
2023-07-17 12:40:55 -0400

    allowed for the songle sign to not be animated

--- components/home/songle-sign/SongleSign.tsx
+++ components/home/songle-sign/SongleSign.tsx
@@ -1,7 +1,38 @@
 import styles from './SongleSign.module.css'
 
-const SongleSign = () => {
+interface SongleSignProps {
+    notAnimated?: boolean
+}
 
+const SongleSign: React.FC<SongleSignProps> = ({ notAnimated }) => {
+
+    if (notAnimated) {
+        return (
+            <div className={styles.container}>
+                <div className={styles["letter-container"]}>
+                    <h1 className={styles["letter-not-animated"]}>S</h1>
+                </div>
+                <div className={styles["not-animated-block1"]} />
+                <div className={styles["letter-container"]}>
+                    <h1 className={styles["other-letter-not-animated"]}>O</h1>
+                </div>
+                <div className={styles["letter-container"]}>
+                    <h1 className={styles["letter-not-animated"]}>N</h1>
+                </div>
+                <div className={styles["not-animated-block2"]} />
+                <div className={styles["letter-container"]}>
+                    <h1 className={styles["other-letter-not-animated"]}>G</h1>
+                </div>
+                <div className={styles["letter-container"]}>
+                    <h1 className={styles["letter-not-animated"]}>L</h1>
+                </div>
+                <div className={styles["not-animated-block3"]} />
+                <div className={styles["letter-container"]}>
+                    <h1 className={styles["other-letter-not-animated"]}>E</h1>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className={styles.container}>
@@ -26,11 +57,6 @@ const SongleSign = () => {
             <div className={styles["letter-container"]}>
                 <h1 className={styles["other-letter"]}>E</h1>
             </div>
-
-
-
-
-
         </div>
     )
 }
--- pages/play.tsx
+++ pages/play.tsx
@@ -1,9 +1,10 @@
 import styles from '../styles/Game.module.css'
+import SongleSign from '@/components/home/songle-sign/SongleSign'
 
 const Play = () => {
     return (
         <div className={styles.container}>
-
+            <SongleSign notAnimated />
         </div>
     )
 }
