197eaa8b82cd6a01acdfd5359e5895658fc2a9e3
jmurphy5613 <jmurphy5613@gmail.com>
2023-07-21 15:46:22 -0400

    added navbar elements

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,10 +1,25 @@
 import styles from './Navbar.module.css'
 import SongleSign from '../songle-sign/SongleSign'
+import Image from 'next/image'
 
 const Navbar = () => {
     return (
         <div className={styles.container}>
             <SongleSign notAnimated />
+            <div className={styles["right-container"]}>
+                <h2 className={styles.link}>Home</h2>
+                <h2 className={styles.link}>Player</h2>
+                <h2 className={styles.link}>Playlist</h2>
+                <h2 className={styles.link}>History</h2>
+                <div className={styles["profile-image-container"]}>
+                    <Image
+                        src="/pfps/johntransparent.png"
+                        alt="profile"
+                        fill
+                        style={{ borderRadius: '100%' }}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
